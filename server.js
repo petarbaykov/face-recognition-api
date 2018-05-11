a48dd89d92467a567e7c5edba65632683ab19f1c
petbaik <petbaik26@gmail.com>
2018-05-12 01:27:48 +0300

    set database

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ var knex = require('knex');
 const database = knex({
 	client:'pg',
 	connection:{
-		host:'127.0.0.1',
-		user:'postgres',
-		password:'root',
-		database:'smart-brain'
+		connectionString:process.env.DATABASE_URL,
+		ssl:true
 	}
 });
 
